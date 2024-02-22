87c8059b50cd883c7b8b1c23e35a7e3754d37bd8
Matthew Ballance <matt.ballance@gmail.com>
2024-02-22 07:54:51 -0800

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/scripts/util.py
+++ src/scripts/util.py
@@ -11,36 +11,6 @@ def setup_build_rtl(dirconfig):
     fwperiph_dma_dir = os.path.abspath(os.path.join(scripts_dir, "../.."))
 
     flow = FlowSim(dirconfig, "mti")
-    # fs = flow.fs
-
-    # fs.add_library(
-    #     os.path.join(fwperiph_dma_dir, "packages/fwvip-wb/src/vip"))
-    # fs.add_library(
-    #     os.path.join(fwperiph_dma_dir, "packages/uvmf-core/src/uvmf/share/uvmf_base_pkg"))
-    # fs.add_library(
-    #     os.path.join(fwperiph_dma_dir, "packages/fwprotocol-defs"))
-    # fs.add_library(
-    #     os.path.join(fwperiph_dma_dir, "packages/fw-wishbone-interconnect"))
-    # fs.add_library(
-    #     os.path.join(fwperiph_dma_dir, "packages/fw-wishbone-sram-ctrl"))
-    # fs.add_library(
-    #     os.path.join(fwperiph_dma_dir, "packages/pss-uvm-util"))
-    # fs.add_library(
-    #     os.path.join(fwperiph_dma_dir, "src/rtl"))
-    # fs.add_library(
-    #     os.path.join(fwperiph_dma_dir, "src/pss"))
-    # fs.add_library(
-    #     os.path.join(fwperiph_dma_dir, "src/verif/pss"))
-    # fs.add_library(
-    #     os.path.join(fwperiph_dma_dir, "src/verif/uvm"))
-    # fs.add_library(
-    #     os.path.join(fwperiph_dma_dir, "src/verif/tb"))
-    
-#    libpath = os.path.join(
-#        zsp_sv.get_libdirs()[0],
-#        "lib" + zsp_sv.get_libs()[0]
-#    )
-#    print("libpath: %s" % libpath)
     
     # pss = ExtPSS.create()
     # pss.pss_core_vlnv = "featherweight-ip::fwperiph_dma.pss.model"
@@ -50,12 +20,7 @@ def setup_build_rtl(dirconfig):
     flow.addFileset("sim", ptv.FSVlnv("uvmf:project_benches:fwperiph_dma_4_chan_tb"))
 
     sim = flow.sim
-#    sim.addFiles(
-#        fs.getFiles("uvmf:project_benches:fwperiph_dma_4_chan_tb"),
-#        {'sv-uvm': True})
-#    sim.dpi_libs.append(libpath)
-    sim.top.add("hdl_top")
-    sim.top.add("hvl_top")
+    flow.sim.top.update({"hdl_top", "hvl_top"})
 
     return flow
 
